3d6c35ac898888e337bf04d23e731d72780f2c97
WhitneyDluhosh <whitneydluhosh@gmail.com>
2018-08-04 11:31:36 -0700

    Update sat2sud.py

--- sat2sud.py
+++ sat2sud.py
@@ -1,23 +1,29 @@
-#Dryden Linden V00849440 Aug 3 2018 
- 
-def sat2sud ():
-  input = sys.argv
+import re
+import sys
+import os
+
+def sat2sud (bubble):
   try: 
-	  file = open(input[1],'r')
-	  input = file.read().split(' ')
-	  file.close()
+    file = open(bubble[1],'r')
+    intake = file.read()
+    intake = intake[3:]
+    file.close()
   except:
-	  print("error opening txt file.")
+    print("error opening txt file.")
   Matrix = [[0 for z in range(9)] for y in range(9)] 
   
-  for line in input:
-    if line.find('-', 0) != -1: 
-    else  
-      Matrix[line[0] -1][line[1] -1] = line[2]
-   
-  for c < 9:
-    for q<9:
+  for line in intake.split(" "):
+    if(int(line) == 0):
+      break
+    if (line.find('-', 0) == -1): 
+      Matrix[int(line[0])-1][int(line[1]) -1] = int(line[2])
+  s = ""   
+  for c in range(9):
+    for q in range (9):
       s = s + str(Matrix[q][c])
-    s = s + "/n"
+    s = s + "\n"
     
   print(s)
+
+bubble = sys.argv
+sat2sud(bubble)
