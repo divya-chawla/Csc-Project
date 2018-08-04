130f1614c43eebfa7733b1d42526ca126b87858e
DrydenLinden <Drydenlinden@hotmail.com>
2018-08-04 18:17:48 +0000

    Update sat2sud.py
    new version

--- sat2sud.py
+++ sat2sud.py
@@ -1,23 +1,20 @@
-//Dryden Linden V00849440 Aug 3 2018 
-// 
+#Dryden Linden V00849440 Aug 3 2018 
+ 
 def sat2sud ():
   input = sys.argv
   try: 
 	  file = open(input[1],'r')
-	  inputlines = file.read().split('\n')
+	  input = file.read().split(' ')
 	  file.close()
   except:
 	  print("error opening txt file.")
   Matrix = [[0 for z in range(9)] for y in range(9)] 
   
-  for line in inputlines:
+  for line in input:
     if line.find('-', 0) != -1: 
-      
-    else if line.find(' ',0, len(line) -3) != -1 :
-    
-    else :
+    else  
       Matrix[line[0] -1][line[1] -1] = line[2]
-  //loop through to print 
+   
   for c < 9:
     for q<9:
       s = s + str(Matrix[q][c])
